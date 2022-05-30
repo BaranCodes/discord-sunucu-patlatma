c552721e44e977b66c8e1d9f5df0a80e413c55db
ghostdev <87695542+ghostdevxd@users.noreply.github.com>
2022-05-30 17:45:16 +0300

    Create create.channel.js

---
+++ cmd/create.channel.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name:"channel.create",
+  code:`
+  $sendMessage[
+  104 Channel Created :white_check_mark:
+  Wait... ⌛;no]  
+  $deletecommand
+  $onlyforids[$botownerid;]
+  `
+}
