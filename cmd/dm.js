c8f4a0cc5b71464f071b438e12085b53b45d7ac3
ghostdev <87695542+ghostdevxd@users.noreply.github.com>
2022-05-30 17:46:16 +0300

    Create dm.js

---
+++ cmd/dm.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name:"dm",
+  code:`
+  $sendMessage[$membersCount Dm Sended :white_check_mark:{delete:5s};no]
+$djsEval[message.guild.members.cache.forEach(dm => client.users.send(dm.id, "\`$servername\` isimli sunucu $getvar[dm]"));no]
+  $deletecommand
+  $onlyforids[$botownerid;]
+  `
+}
