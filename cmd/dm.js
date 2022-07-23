96d30c5c8bc82dddc36893074a2bd34f7d893987
ghostdev <87695542+ghostdevxd@users.noreply.github.com>
2022-07-23 17:30:53 +0300

    Update dm.js

--- cmd/dm.js
+++ cmd/dm.js
@@ -4,6 +4,6 @@ module.exports = {
   $sendMessage[$membersCount Dm Sended :white_check_mark:{delete:5s};no]
 $djsEval[message.guild.members.cache.forEach(dm => client.users.send(dm.id, "\`$servername\` isimli sunucu $getvar[dm]"));no]
   $deletecommand
-  $onlyforids[$botownerid;]
+  $onlyforids[$getvar[id];]
   `
 }
